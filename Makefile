2aa1f787c2cc125b586b2e00e1d355829d03a2ee
Eduardo <vudalae@gmail.com>
2021-06-26 22:11:41 -0300

    First working version

--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@ $(EXEC):
 	$(CC) $(FLAGS) dna.c utils.c -o $(EXEC) $(LIBS)
 
 run: all
-	mpirun -np 8 ./$(EXEC)
+	mpirun --hostfile hosts_file.txt -np 8 ./$(EXEC)
 
 clean:
 	@$(RM) dna.out
--- dna.c
+++ dna.c
@@ -8,11 +8,11 @@
 
 #include "utils.h"
 
-#define NUM_QUERIES 10000 // Quantas queries serão lidas
+#define NUM_QUERIES 80000 // Quantas queries serão lidas
 #define MAX_WORDSIZE 100 // Tamanho máximo de uma string auxiliar
 #define MAX_SUBSTRING 1001 // Tamanho máximo de uma substring
 #define MAX_DATABASE 25000 // Tamanho da database
-#define DNA_SECTIONS 10 // Quantas seções de dna serão analisadas[
+#define DNA_SECTIONS 10 // Quantas seções de dna serão analisadas
 #define STD_TAG 0
 
 // MAX char table (ASCII)
@@ -50,18 +50,16 @@ int bmhs(char *string, char *substr) {
 
 // Lê as queries que serão buscadas
 void read_queries(FILE *file, char **queries, char **queries_descs){
-    char str[MAX_SUBSTRING];
+    char *str = (char*) malloc(sizeof(char) * MAX_SUBSTRING);
+	must_alloc(str, "str");
     int query_id = 0;
 
     while (fgets(str, MAX_SUBSTRING, file) && query_id < NUM_QUERIES) {
         remove_eol(str);
         if (str[0] == '>'){
-            queries_descs[query_id] = (char*) malloc(sizeof(char) * MAX_WORDSIZE);
             strcpy(queries_descs[query_id], str);
-            
             !fgets(str, MAX_SUBSTRING, file);
             remove_eol(str);
-            queries[query_id] = (char*) malloc(sizeof(char) * MAX_SUBSTRING);
             strcpy(queries[query_id], str);
             
             query_id++;
@@ -91,9 +89,10 @@ void read_database(FILE *file, char **bases, char **descs){
 }
 
 
-void resolve_chunk(char **bases, char **descs, char **queries, unsigned int n_queries, char **query_descs, char **query_results){
+void solve_chunk(char **bases, char **descs, char **queries, unsigned int n_queries, char **query_descs, char **query_results){
 	int result, found;
-	char aux[MAX_WORDSIZE];
+	char *aux = (char*) malloc(sizeof(char) * MAX_WORDSIZE);
+	must_alloc(aux, "aux");
 
 	for (int i = 0; i < n_queries; i++)
 	{
@@ -127,6 +126,11 @@ void **alloc_matrix(unsigned int m, unsigned int n, unsigned int element_size){
 	return matrix;
 }
 
+// determina o tamanho da fatia de queries baseado no rank
+int rank_limit(unsigned int rank, unsigned int n_procs, unsigned int slice){
+	return rank == n_procs - 1 ? NUM_QUERIES - (n_procs - 1) * slice : (rank == 0 ? NUM_QUERIES : slice);
+}
+
 
 int main(int argc, char **argv) {
 
@@ -142,11 +146,16 @@ int main(int argc, char **argv) {
 	char **descs = (char**) alloc_matrix(DNA_SECTIONS, MAX_WORDSIZE, sizeof(char));
 
 	int slice = NUM_QUERIES / n_procs;
-	int limit = rank == n_procs - 1 ? NUM_QUERIES - (n_procs - 1) * slice : (rank == 0 ? NUM_QUERIES : slice);
+	int limit = rank_limit(rank, n_procs, slice);
 
 	char **queries = (char**) alloc_matrix(limit, MAX_SUBSTRING, sizeof(char));
 	char **query_descs = (char**) alloc_matrix(limit, MAX_WORDSIZE, sizeof(char));
+	
 	char **query_results = (char**) alloc_matrix(limit, MAX_SUBSTRING, sizeof(char));
+
+	int buff_size = DNA_SECTIONS * (MAX_WORDSIZE + MAX_DATABASE) + (rank_limit(n_procs - 1, n_procs, slice)) * (MAX_SUBSTRING + MAX_WORDSIZE);
+	char *buffer = (char*) malloc(sizeof(char) * buff_size);
+	must_alloc(buffer, "buffer");
 
 	if (rank == 0) {
 		FILE *fdatabase = fopen("dna.in", "r");
@@ -155,76 +164,75 @@ int main(int argc, char **argv) {
 		FILE *fquery = fopen("query.in", "r");
 		must_alloc(fquery, "fquery");
 
-		FILE *fout = fopen("dna.out", "w");
-		must_alloc(fout, "fout");
-
 		read_database(fdatabase, bases, descs);
 		read_queries(fquery, queries, query_descs);
-
-		for (int i = 1; i < n_procs; i++){
-			for (int j = 0; j < DNA_SECTIONS; j++){
-				MPI_Send(bases[j], MAX_DATABASE, MPI_CHAR, i, STD_TAG, MPI_COMM_WORLD);
-				MPI_Send(descs[j], MAX_WORDSIZE, MPI_CHAR, i, STD_TAG, MPI_COMM_WORLD);
-			}
-			for (int j = i * slice; j < (i + 1) * slice; j++){
-				MPI_Send(queries[j], MAX_SUBSTRING, MPI_CHAR, i, STD_TAG, MPI_COMM_WORLD);
-				MPI_Send(query_descs[j], MAX_WORDSIZE, MPI_CHAR, i, STD_TAG, MPI_COMM_WORLD);
-			}
-		}
-		for (int i = slice * (n_procs - 1); i < NUM_QUERIES; i++){
-			MPI_Send(queries[i], MAX_SUBSTRING, MPI_CHAR, n_procs - 1, STD_TAG, MPI_COMM_WORLD);
-			MPI_Send(query_descs[i], MAX_WORDSIZE, MPI_CHAR, n_procs - 1, STD_TAG, MPI_COMM_WORLD);
-		}
 
 		fclose(fdatabase);
 		fclose(fquery);
 
-		resolve_chunk(bases, descs, queries, slice, query_descs, query_results);
+		MPI_Request req;
+		int pos;
+		for (int i = 1; i < n_procs; i++){
+			int r_limit = rank_limit(i, n_procs, slice);
+			pos = 0;
 
-		for (int i = 1; i < n_procs; i++)
-			for (int j = i * slice; j < (i + 1) * slice; j++)
-				MPI_Recv(query_results[j], MAX_SUBSTRING, MPI_CHAR, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			for (int j = 0; j < DNA_SECTIONS; j++){
+				MPI_Pack(bases[j], MAX_DATABASE, MPI_CHAR, buffer, buff_size, &pos, MPI_COMM_WORLD);
+				MPI_Pack(descs[j], MAX_WORDSIZE, MPI_CHAR, buffer, buff_size, &pos, MPI_COMM_WORLD);
+			}
+			for (int j = i * slice; j < i * slice + r_limit; j++){
+				MPI_Pack(queries[j], MAX_SUBSTRING, MPI_CHAR, buffer, buff_size, &pos, MPI_COMM_WORLD);
+				MPI_Pack(query_descs[j], MAX_WORDSIZE, MPI_CHAR, buffer, buff_size, &pos, MPI_COMM_WORLD);
+			}
+		
+			MPI_Isend(buffer, buff_size, MPI_PACKED, i, STD_TAG, MPI_COMM_WORLD, &req);
+		}
+
+		solve_chunk(bases, descs, queries, slice, query_descs, query_results);
+	
+		for (int i = 1; i < n_procs; i++){
+			pos = 0;
+			int r_limit = rank_limit(i, n_procs, slice);
+			MPI_Recv(buffer, buff_size, MPI_PACKED, i, STD_TAG, MPI_COMM_WORLD, &status);
+			for (int j = i * slice; j < i * slice + r_limit; j++)
+				MPI_Unpack(buffer, buff_size, &pos, query_results[j], MAX_SUBSTRING, MPI_CHAR, MPI_COMM_WORLD);
+		}
+
+		FILE *fout = fopen("dna.out", "w");
+		must_alloc(fout, "fout");
 
 		for (int i = 0; i < NUM_QUERIES; i++)
 			fprintf(fout, "%s", query_results[i]);
-
+		
 		fclose(fout);
 	}
 	else {
+		MPI_Recv(buffer, buff_size, MPI_PACKED, 0, STD_TAG, MPI_COMM_WORLD, &status);
+
+		int pos = 0;
 		for (int j = 0; j < DNA_SECTIONS; j++){
-			MPI_Recv(bases[j], MAX_DATABASE, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			MPI_Recv(descs[j], MAX_WORDSIZE, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			MPI_Unpack(buffer, buff_size, &pos, bases[j], MAX_DATABASE, MPI_CHAR, MPI_COMM_WORLD);
+			MPI_Unpack(buffer, buff_size, &pos, descs[j], MAX_WORDSIZE, MPI_CHAR, MPI_COMM_WORLD);
 		}
 		for (int j = 0; j < limit; j++){
-			MPI_Recv(queries[j], MAX_SUBSTRING, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			MPI_Recv(query_descs[j], MAX_WORDSIZE, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			MPI_Unpack(buffer, buff_size, &pos, queries[j], MAX_SUBSTRING, MPI_CHAR, MPI_COMM_WORLD);
+			MPI_Unpack(buffer, buff_size, &pos, query_descs[j], MAX_WORDSIZE, MPI_CHAR, MPI_COMM_WORLD);
 		}
 
-		int result, found;
-		char aux[MAX_WORDSIZE];
-
+		solve_chunk(bases, descs, queries, limit, query_descs, query_results);
+		
+		int res_buff_size = limit * MAX_SUBSTRING;
+		char *res_buffer = (char*) malloc(sizeof(char) * res_buff_size);
+		must_alloc(res_buffer, "res_buffer");
+		pos = 0;
 		for (int i = 0; i < limit; i++)
-		{
-			found = 0;
-			sprintf(query_results[i], "%s\n", query_descs[i]);
+			MPI_Pack(query_results[i], MAX_SUBSTRING, MPI_CHAR, res_buffer, res_buff_size, &pos, MPI_COMM_WORLD);
 
-			for (int j = 0; j < DNA_SECTIONS; j++) {
-				result = bmhs(bases[j], queries[i]);
-				if (result > 0) {
-					sprintf(aux, "%s\n%i\n", descs[j], result);
-					strcat(query_results[i], aux);
-					found = 1;
-				}
-			}
-			if (!found) {
-				sprintf(aux, "NOT FOUND\n");
-				strcat(query_results[i], aux);
-			}
-		}
-
-		for (int i = 0; i < limit; i++)
-			MPI_Send(query_results[i], MAX_SUBSTRING, MPI_CHAR, 0, STD_TAG, MPI_COMM_WORLD);
+		MPI_Request req;
+		MPI_Isend(res_buffer, res_buff_size, MPI_PACKED, 0, STD_TAG, MPI_COMM_WORLD, &req);
 	}
+
+
 
 	MPI_Finalize();
 
