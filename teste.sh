e7a6a6fc22eca0a8d1ba9cbacf86647037ac10a7
Eduardo <vudalae@gmail.com>
2021-06-27 22:50:41 -0300

    Test script adaptation

--- teste.sh
+++ teste.sh
@@ -1,16 +1,12 @@
 # Eduardo Vudala Senoski GRR20195689
 
-export OMP_PROC_BIND=true
-export OMP_DYNAMIC=false
-
 END=10
 testar () {
-    export OMP_NUM_THREADS=$1
     SUM=0
     for i in $(seq 1 $END);
     do
         start=$(date +%s.%N)
-        $2
+        mpirun --hostfile hosts_file.txt -np $1 $2
         dur=$(echo "$(date +%s.%N) - $start" | bc)
         SUM=$(echo "$SUM + $dur" | bc)
     done
@@ -18,4 +14,4 @@ testar () {
     echo "scale=9; $SUM / $END" | bc
 }
 
-testar 8 ./dna
+testar 8 ./dna80
